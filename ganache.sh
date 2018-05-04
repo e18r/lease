287b481de1df7b0ca5e8033990d434514b571754
Emilio Silva Schlenker <emilioss@gmail.com>
2018-05-04 17:31:21 -0500

    test setting

---
+++ ganache.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+ganache-cli -p 8875
---
+++ rinkeby.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+geth --datadir "/media/tape/geth/rinkeby" --networkid 4 --rpcport 8555 --wsport 8556 --port 30313 --rpc --unlock fa39e3acc25d0ae3a8016b1ea0a8079a4fa9accf,0f66169fdb7724dc13264beae174baa878517d6c,58dd236f25927f4238d6d39a1c02552d49bab62a,782866c1110c0d67a19efdae1758d9ebe99b3b89
