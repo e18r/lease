ab2b838cfad3e3d47986d066d998dcd55852756e
Emilio Silva Schlenker <emilioss@gmail.com>
2018-05-25 12:43:11 -0500

    new contract also fetches the contract

--- app/index.js
+++ app/index.js
@@ -1,6 +1,5 @@
 import Web3 from "web3";
-import { submitContract } from "./creation.js";
-import { insertInput, fetch, set, setResult } from "./lib.js";
+import { insertInput, get, set, setResult, deploy, fetch } from "./lib.js";
 
 let web3 = new Web3(Web3.givenProvider || "http://127.0.0.1:8875");
 
@@ -30,6 +29,32 @@ async function fetchContract() {
   }
   catch(error) {
     setResult("failure", "info", error);
+  }
+}
+
+async function submitContract() {
+  try {
+    let owner = await web3.eth.getCoinbase();    
+    let tenant = get("address", "creation", "tenant");
+    let start = get("date", "creation", "start");
+    let fee = get("amount", "creation", "fee");
+    let deposit = get("amount", "creation", "deposit");
+    let lease = await deploy(web3, owner, tenant, start, fee, deposit)
+    let actualTenant = await (await lease.methods.tenant()).call();
+    let actualStart = await (await lease.methods.start()).call();
+    let actualFee = await (await lease.methods.fee()).call();
+    let actualDeposit = await (await lease.methods.deposit()).call();
+    set("address", "creation", "tenant", actualTenant);
+    set("date", "creation", "start", actualStart);
+    set("amount", "creation", "fee", actualFee);
+    set("amount", "creation", "deposit", actualDeposit);
+    setResult("success", "creation", "contract created at " + lease._address);
+    let contract = document.getElementById("info contract");
+    contract.value = lease._address;
+    fetchContract();
+  }
+  catch(error) {
+    setResult("failure", "creation", error)
   }
 }
 
